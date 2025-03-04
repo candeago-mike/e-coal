92dd0ea028e75cc5e9fbd43f6aa4662a6c0abc56
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2025-03-04 09:59:45 +0100

    try push css styling

--- ecoal/ecoal25/client/pages/login.jsx
+++ ecoal/ecoal25/client/pages/login.jsx
@@ -1,20 +1,40 @@
-import { NavLink } from "react-router";
-import React from "react";
+import { NavLink } from "react-router"
+import React from "react"
+
 export const Login = () => {
     return (
-        <div className="flex flex-col items-center gap-4">
-            <h1 className="text-3xl">Login for Any Trip</h1>
-            <label>email</label>
-            <input className="border b-1 border-black" type="email" />
-            <label >password</label>
-            <input className="border b-1 border-black" type="password" />
-            <NavLink to="/register">
-                <button>register</button>
-            </NavLink>
-
-            <NavLink to="/anytrypage">
-                <button className="bg-blue-500 border b-1 border-black">login</button>
-            </NavLink>
+        <div className="h-screen w-screen flex items-center justify-center bg-gray-100">
+            <div className="w-full max-w-xs">
+                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                            Email
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="email" type="text" placeholder="Email"/>
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                            Password
+                        </label>
+                        <input
+                            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                            id="password" type="password" placeholder="******"/>
+                        <p className="text-red-500 text-xs italic">Please enter a password.</p>
+                    </div>
+                    <div className="flex justify-center">
+                        <button
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            type="button">
+                            Login
+                        </button>
+                    </div>
+                </form>
+                <p className="text-center text-gray-500 text-xs">
+                    &copy;2025 AnyTrip Corp. All rights reserved.
+                </p>
+            </div>
         </div>
-    );
-};
+    )
+}
