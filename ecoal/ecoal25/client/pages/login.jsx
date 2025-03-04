e600132e276839dc071fe35a6d04fb9c91ac5054
Fábio Pinto <fabio.rafael7@hotmail.com>
2025-03-04 10:01:03 +0000

    fix(small error) routing

--- ecoal/ecoal25/client/pages/login.jsx
+++ ecoal/ecoal25/client/pages/login.jsx
@@ -38,7 +38,7 @@ export const Login = () => {
                         </p>
                     </div>
                     <div className="flex justify-center">
-                        <NavLink to="/anytryp">
+                        <NavLink to="/anytrip">
                             <button
                                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                                 type="button"
