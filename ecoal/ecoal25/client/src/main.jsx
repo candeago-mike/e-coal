14772693bc4ad05f01af9cc3cb13f2211b74dd2b
Fábio Pinto <fabio.rafael7@hotmail.com>
2025-03-05 10:38:30 +0000

    add(routing) fix

--- ecoal/ecoal25/client/src/main.jsx
+++ ecoal/ecoal25/client/src/main.jsx
@@ -1,31 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { CookiesProvider, useCookies } from "react-cookie";
+import { Cookies, CookiesProvider } from "react-cookie";
 import { Login } from "../pages/login.jsx";
 import { MainPage } from "../pages/mainpage.jsx";
 import { Register } from "../pages/register.jsx";
 import { About } from "../pages/about.jsx";
 import { ArticlePage } from "../src/shared/articlePage";
+import ProtectedRoute from "./shared/protectedRoute.jsx";
 
-const ProtectedRoute = ({ children }) => {
-    const [cookies] = useCookies(["user"]); 
-    
-    return cookies.user?.token ? children : <Navigate to="/" replace />;
-};
+function getCookieValue(name) 
+    {
+      const regex = new RegExp(`(^| )${name}=([^;]+)`)
+      const match = document.cookie.match(regex)
+      if (match) {
+        return match[2]
+      }
+   }
 
 const root = document.getElementById("root");
+const getAccessToken = () => {
+  return getCookieValue("accessToken")!= undefined
 
+}
+
+const isAuthenticated = () => {
+  return !!getAccessToken();
+}
 ReactDOM.createRoot(root).render(
   <CookiesProvider defaultSetOptions={{ path: '/' }}>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/anytrip" element={<ProtectedRoute><MainPage /></ProtectedRoute>} />
-        <Route path="/about" element={<ProtectedRoute><About /></ProtectedRoute>} />
-        <Route path="/article/:id" element={<ProtectedRoute><ArticlePage /></ProtectedRoute>} />
-
+        {isAuthenticated() ? <Route path="/anytrip" element={<MainPage />} /> :null  }
+        {isAuthenticated() ? <Route path="/about" element={<About />} /> :null}
+        {isAuthenticated() ? <Route path="/article/:id" element={<ArticlePage />} /> :null}
+        <Route path="*" element={<Login/>} />
       </Routes>
     </BrowserRouter>
   </CookiesProvider>
