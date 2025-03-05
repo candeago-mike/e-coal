9d3ce919de2eb1d1818a80e9c0dfbb9ac4080e63
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2025-03-05 10:51:02 +0100

    change mickey name

--- ecoal/ecoal25/client/pages/about.jsx
+++ ecoal/ecoal25/client/pages/about.jsx
@@ -11,14 +11,15 @@ export const About = () => {
                 <img
                     src="src/images/AboutUs.jpg"
                     alt="Team AnyTrip"
-                    className="w-full h-full object-cover opacity-60"
+                    className="w-full h-full object-cover opacity-70"
                 />
-                <div className="absolute inset-0 flex flex-col items-center justify-center">
+                <div className="absolute inset-0 flex flex-col items-center justify-center text-center w-full px-4">
                     <h1 className="text-2xl text-[#090920] font-bold">ABOUT US</h1>
+                    {/*<h2 className="font-bold text-2xl md:text-3xl text-[#090920]">By Team AnyTrip</h2>*/}
                     <i><h2 className="font-bold text-4xl text-[#090920]">Team AnyTrip</h2></i>
                 </div>
-
             </div>
+
             <div className="relative flex flex-col items-center bg-[#ffffff] min-h-screen p-6 text-[#090920]">
                 {/* Background Section */}
                 <div className="w-4/5 bg-[#F8E6D4] p-8 rounded-lg shadow-lg relative mt-10">
@@ -42,7 +43,7 @@ export const About = () => {
                     <div className="px-8">
                         <strong><p className="text-xl text-[#090920] mb-4">A Postcard From AnyTrip</p></strong>
                         <p className="text-justify text-[#090920] mb-4">
-                            We are Carlos, Cheryl, Clea, Mickey, Hanna, and Fabio – a group of travel enthusiasts from different corners of the world, united by our passion for discovering new places and sharing our experiences.
+                            We are Carlos, Cheryl, Clea, Mike, Hanna, and Fabio – a group of travel enthusiasts from different corners of the world, united by our passion for discovering new places and sharing our experiences.
                         </p>
                         <p className="text-justify text-[#090920] mb-4">
                             As part of our Erasmus project, we created AnyTrip, a mobile app designed to make travel planning easier, faster, and more personal.
