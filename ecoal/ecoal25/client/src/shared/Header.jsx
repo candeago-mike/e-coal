2b701baa702bebd6269a56128ef14c6a1900c1f4
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2025-03-06 12:00:07 +0100

    put logo into navbar

--- ecoal/ecoal25/client/src/shared/Header.jsx
+++ ecoal/ecoal25/client/src/shared/Header.jsx
@@ -5,14 +5,17 @@ export const Header = () => {
     return (
         <header className="w-full flex justify-between bg-[#090920] items-center py-4 text-[#E8D1C1] px-6">
             <NavLink to="/anytrip" className="text-[#E8D1C1] font-semibold hover:text-[#ffffff]">
-                <h1 className="text-xl font-bold">AnyTrip</h1>
+               <img
+                   src="/src/images/Logo.png"
+                   alt="AnyTrip" className="w-35"
+               />
             </NavLink>
             <nav className="flex gap-6">
                 <NavLink to="/anytrip" className="text-[#E8D1C1] font-semibold hover:text-[#ffffff]">
                     Home
                 </NavLink>
                 <NavLink to="/about" className="text-[#E8D1C1] hover:text-[#ffffff] font-semibold">
-                    About Us
+                    About
                 </NavLink>
                 <NavLink to="/" className="text-[#E8D1C1] hover:text-[#ffffff] font-semibold">
                     Logout
